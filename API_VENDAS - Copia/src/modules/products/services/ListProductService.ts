6f55c28a950e3cfdf8a972288d53ce77a77323c2
Manuela <manuelaf1131@gmail.com>
2026-04-15 16:20:49 -0300

    apaguei copia

--- API_VENDAS - Copia/src/modules/products/services/ListProductService.ts
+++
@@ -1,9 +0,0 @@
-import { AppDataSource } from "@shared/typeorm/data-source";
-import Product from "../typeorm/entities/Product";
-
-export default class listProductService{
-    public async execute(): Promise<Product[]>{
-        const productRepository = AppDataSource.getRepository(Product);
-        return await productRepository.find()
-    }
-}
